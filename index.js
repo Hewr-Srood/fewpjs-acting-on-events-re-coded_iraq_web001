9346b8ee025e3ad03843f5f88dab98a1cb8b203d
Hewr Srood <hewr_s@outlook.com>
2020-05-20 10:37:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,20 +8,25 @@ dodger.style.bottom = "100px";
 document.addEventListener("keydown", function(e) {
   console.log(e.key);
 });
-document.addEventListener("keydown", function(e) {
-  if (e.key === "ArrowLeft") {
-    let leftNumbers = dodger.style.left.replace("px", "");
-    let left = parseInt(leftNumbers, 10);
-
-    dodger.style.left = `${left - 1}px`;
-  }
-});
 function moveDodgerLeft() {
   let leftNumbers = dodger.style.left.replace("px", "");
   let left = parseInt(leftNumbers, 10);
 
   if (left > 0) {
     dodger.style.left = `${left - 1}px`;
+  }
+}
+document.addEventListener("keydown", function(e) {
+  if (e.key === "ArrowRight") {
+    moveDodgerRight();
+  }
+});
+function moveDodgerRight() {
+  let leftNumbers = dodger.style.left.replace("px", "");
+  let left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left + 1}px`;
   }
 }
 document.addEventListener("keydown", function(e) {
